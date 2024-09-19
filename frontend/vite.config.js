c0e332b823bc0e1757df5431d007c9a96d76ecc3
Emma Jensen <emmasarahlinnea@gmail.com>
2024-09-19 11:59:13 +0200

    PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './public/manifest.json';
+
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: 'build',
     },
-    plugins: [react()],
+    plugins: [react(),
+      VitePWA({ 
+         registerType: 'autoUpdate',
+         includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+         manifest: manifest 
+       })
+   
+    ],
   };
 });

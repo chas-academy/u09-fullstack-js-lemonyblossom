c680d038acbcd0be45b28cf454f464c5df02b240
Emma Jensen <emmasarahlinnea@gmail.com>
2024-09-25 16:39:43 +0200

    fix, email reset password, not completed, continue

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User'); 
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
 
 // REGISTER

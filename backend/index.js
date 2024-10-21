7c4c36b1f5792eda7b119c428c9dd258eea4b20c
Emma Jensen <emmasarahlinnea@gmail.com>
2024-10-21 20:33:30 +0200

    .env add apiurl, added both localhost:3000 and feelstate netlify as origin for cors

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://feelstate.netlify.app',
+  origin: ['https://feelstate.netlify.app', 'http://localhost:3000'], 
   credentials: true,  
 }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log('Connected to MongoDB');
   })

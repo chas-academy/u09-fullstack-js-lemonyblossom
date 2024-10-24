a57d992f0c2f323c03c3c1f134c1b17f5348ebaf
Emma Jensen <emmasarahlinnea@gmail.com>
2024-10-24 13:28:17 +0200

    moodcounter width

--- frontend/src/components/charts/MoodCounterChart.jsx
+++ frontend/src/components/charts/MoodCounterChart.jsx
@@ -70,7 +70,7 @@ const MoodCounterChart = () => {
 
   return (
     <div className="md:w-3/4 lg:w-3/5 mx-auto max-width-[600px]">
-      <div className="max-w-full h-full">
+      <div className="max-w-1/2 ">
         <h2 className="text-xl text-center mb-4">Monthly Moods</h2>
         <Doughnut
           data={chartData}
